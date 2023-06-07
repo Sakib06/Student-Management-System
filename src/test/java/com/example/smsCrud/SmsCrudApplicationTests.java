da25d4d192033956b305186a8600a27b7f0aaa58
Sakib06 <asm.nazmus.sakib.fahim@g.bracu.ac.bd>
2023-06-08 00:30:32 +0600

    first commit

---
+++ src/test/java/com/example/smsCrud/SmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smsCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
